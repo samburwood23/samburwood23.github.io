e476d34027b3b7add2ee90b597619969a890a63a
Samantha Burwood <152337271+samburwood23@users.noreply.github.com>
2025-11-14 19:58:55 +0000

    Refactor event listener for hamburger menu

--- script.js
+++ script.js
@@ -1,7 +1,6 @@
+const hamburger = document.getElementById("hamburger");
+const navMenu = document.getElementById("nav-menu");
 
-const hamburger = document.getElementById('hamburger');
-const navMenu = document.getElementById('nav-menu');
-
-hamburger.addEventListener('click', () => {
-    navMenu.classList.toggle('show');
+hamburger.addEventListener("click", () => {
+    navMenu.querySelector("ul").classList.toggle("show");
 });
