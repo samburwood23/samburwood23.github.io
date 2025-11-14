ef98d221775dbd42ba44ca8240e67d0a95958559
Samantha Burwood <152337271+samburwood23@users.noreply.github.com>
2025-11-14 22:48:22 +0000

    Update script.js

--- script.js
+++ script.js
@@ -1,7 +1,6 @@
 // Mobile sidebar toggle
 const hamburger = document.querySelector('.hamburger');
 const body = document.body;
-
 hamburger.addEventListener('click', () => {
     body.classList.toggle('sidebar-open');
 });
